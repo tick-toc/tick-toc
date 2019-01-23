533543865f95c097a57a73060c637b657597be49
celipas <celipas@gmail.com>
2019-01-23 14:45:43 -0600

    working on import schema

--- server/index.js
+++ server/index.js
@@ -1,30 +1,26 @@
+const importSchema = require('graphql-import').importSchema
 const { GraphQLServer } = require('graphql-yoga')
+const { prisma } = require('./generated/prisma-client')
+const Query = require('./resolvers/Query')
+const Mutation = require('./resolvers/Mutation')
+const Subscription = require('./resolvers/Subscription')
 
-//1 this will move to schema file...this is our schema
-// Every GraphQL schema has three special root types,
-// these are called Query, Mutation and Subscription
-// root fields are below the root types
-// When the type of a root field is an object type,
-// you can further expand the query (or mutation/subscription)
-// with fields of that object type. The expanded part is
-// called selection set
-const typeDefs = `
-    type Query {
-        info: String!
-    }
-`
 
-//2 this will move to a resolvers file
 const resolvers = {
-    Query: {
-        info: () => `This is the API of our Bomb Game`
-    }
+    // Mutation,
+    Query,
+    // Subscription
 }
 
 // 3 this will stay here
 const server = new GraphQLServer({
-    typeDefs,
+    typeDefs: importSchema('./schema.graphql'),
+    // typeDefs,
     resolvers,
+    context: request => ({
+        ...request,
+        prisma
+    })
 })
 
 server.start(() => console.log(`Server is running on http://localhost:4000`))
---
+++ server/resolvers/Mutation.js
@@ -0,0 +1,5 @@
+const Mutation = {
+
+}
+
+module.exports = Mutation
