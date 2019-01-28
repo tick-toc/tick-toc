9de5c0d87f8891f04cb78ce2a1395c53d987fecb
celipas <celipas@gmail.com>
2019-01-28 08:29:17 -0600

    commiting before pulling down evans branch

--- src/components/Bomb/Bomb.js
+++ src/components/Bomb/Bomb.js
@@ -321,21 +321,9 @@ class Bomb extends Component {
 
 
     }
-    var time = 300
-    var counter = 0
-    function timeIt() {
-      counter++
-      var timeLeft = time - counter
 
-      var min = Math.floor(timeLeft / 60)
-      var sec = Math.floor(timeLeft % 60)
 
-      // console.log(min + ':' + sec)
-      const str = (min + ':' + sec).toString()
-      // console.log('str', str)
-      return str
-    }
-    setInterval(timeIt, 1000);
+
 
     function onDocumentMouseDown(event) {
       // the following line would stop any other event handler from firing
@@ -362,37 +350,7 @@ class Bomb extends Component {
         clock.remove(intersects[0].object)
         // clock.add(timeIt())
 
-        let fontLoader = new THREE.FontLoader();
-        fontLoader.load('fonts/Digital-7_Regular.json', function (font) {
 
-          let textGeo = new THREE.TextGeometry(timeIt(), {
-            font: font,
-            size: 12,
-            height: 1,
-            curveSegments: 2,
-            bevelEnabled: true,
-            bevelThickness: 1,
-            bevelSize: 0.1,
-            bevelSegments: 5
-          });
-          let textMat = new THREE.MeshPhongMaterial({
-            color: 0xff1111,
-            shininess: 100,
-          });
-          if (textGeo) {
-            textGeo.computeBoundingBox();
-            textGeo.computeVertexNormals();
-            let text = new THREE.Mesh(textGeo, textMat)
-            text.scale.set(0.06, 0.06, 0.06);
-            text.position.x = 0.16;
-            text.position.y = -0.68;
-            text.position.z = 0.8;
-            text.rotation.y = Math.PI / 2;
-            text.name = "ClockDisplay"
-            clock.add(text)
-            targetList.push(text)
-          }
-        });
 
         console.log(scene)
         console.log(document)
--- src/components/Bomb/Timer/Timer.js
+++ src/components/Bomb/Timer/Timer.js
@@ -1,14 +1,54 @@
-// //344
-// if (intersects[0].object.name === 'ClockDisplay') {
-//     // intersects[0].object.geometry.parameters.text = '4:00'
-//     console.log(intersects[0].object.geometry.parameters.text)
-// }
 
-// //238
-// text.name = "ClockDisplay"
-// clock.add(text)
-// targetList.push(text)
+import * as THREE from 'three';
 
-function timeIt(time) {
+var time = 300
+var counter = 0
+var clock
+var targetList = [];
 
+function timeIt() {
+    counter++
+    var timeLeft = time - counter
+
+    var min = Math.floor(timeLeft / 60)
+    var sec = Math.floor(timeLeft % 60)
+
+    // console.log(min + ':' + sec)
+    const str = (min + ':' + sec).toString()
+    // console.log('str', str)
+    return str
 }
+
+setInterval(timeIt, 1000);
+
+let fontLoader = new THREE.FontLoader();
+fontLoader.load('fonts/Digital-7_Regular.json', function (font) {
+
+    let textGeo = new THREE.TextGeometry(timeIt(), {
+        font: font,
+        size: 12,
+        height: 1,
+        curveSegments: 2,
+        bevelEnabled: true,
+        bevelThickness: 1,
+        bevelSize: 0.1,
+        bevelSegments: 5
+    });
+    let textMat = new THREE.MeshPhongMaterial({
+        color: 0xff1111,
+        shininess: 100,
+    });
+    if (textGeo) {
+        textGeo.computeBoundingBox();
+        textGeo.computeVertexNormals();
+        let text = new THREE.Mesh(textGeo, textMat)
+        text.scale.set(0.06, 0.06, 0.06);
+        text.position.x = 0.16;
+        text.position.y = -0.68;
+        text.position.z = 0.8;
+        text.rotation.y = Math.PI / 2;
+        text.name = "ClockDisplay"
+        clock.add(text)
+        targetList.push(text)
+    }
+});
