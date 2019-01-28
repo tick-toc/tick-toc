14169cf469f937e5d35d9f363b490633a7c558ed
Evan Alto <evanalto@Evans-MacBook.local>
2019-01-28 11:26:36 -0600

    created user login/signup view

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,40 @@
-import React, { Component } from 'react';
-import Bomb from './Bomb/Bomb';
-// import TestSvg from './TestSvg'
-import '../styles/App.css';
+import React, { Component } from 'react'
+import Bomb from './Bomb/Bomb'
+import '../styles/App.css'
+import Login from './Login'
+import {Switch, Route } from 'react-router-dom'
 
 class App extends Component {
+
+  componentDidMount() {
+    // Check if user is logged in
+  }
+
   render() {
     return (
       <div className="App">
-        <Bomb />
-        {/* <TestSvg /> */}
+        <h1 className="title">TICK TOC</h1>
+        <Switch>
+          <Route exact path="/" component={Login} />
+          {/* ^^ Will render main-menu or Login, if logged in or not */}
+          <Route exact path="/new-game" component={Login}/> 
+          {/* ^^^ Will render game menu / game options */}
+          <Route exact path="/previous-games" component={Login}/> 
+          {/* ^^^ Will render previous games */}
+          <Route exact path="/leaderboards" component={Login}/> 
+          {/* ^^^ Will render leaderboards*/}
+          <Route exact path="/diffusing" component={Bomb} />
+
+          <Route exact path="/recap" component={Bomb} />
+          {/* ^^^ Will render post game results */}
+          <Route component={Login} />
+        </Switch>
       </div>
     );
   }
 }
+{/* 
+  Conditional Checking if isLogged or not
+*/}
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,27 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'
-import { ApolloProvider } from 'react-apollo'
-import { ApolloClient } from 'apollo-client'
-import { createHttpLink } from 'apollo-link-http'
-import { InMemoryCache } from 'apollo-cache-inmemory'
 import './styles/index.css';
 import App from './components/App.js';
 import * as serviceWorker from './serviceWorker';
 
-const httpLink = createHttpLink({
-    uri: "http://localhost:4000"
-})
-
-const client = new ApolloClient({
-    link: httpLink,
-    cache: new InMemoryCache()
-})
 ReactDOM.render(
     <BrowserRouter>
-        <ApolloProvider client={client}>
             <App />
-        </ApolloProvider>
     </BrowserRouter>
     , document.getElementById('root'));
 
