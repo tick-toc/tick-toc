3d4ccc7362b55d883eaf158a874b6562c2ae4858
Evan Alto <evanalto@Evans-MacBook.local>
2019-01-28 15:10:01 -0600

    fixed strike logic

--- src/components/App.js
+++ src/components/App.js
@@ -2,27 +2,33 @@ import React, { Component } from 'react'
 import Bomb from './Bomb/Bomb'
 import '../styles/App.css'
 import Login from './Login'
-import {Switch, Route } from 'react-router-dom'
+import Main from './Main'
+import NewGame from './NewGame'
+import {Switch, Route, withRouter } from 'react-router-dom'
 
 class App extends Component {
 
   componentDidMount() {
     // Check if user is logged in
+    console.log(this.props,'<<PROPS')
   }
 
   render() {
     return (
       <div className="App">
-        <h1 className="title">TICK TOC</h1>
+      {
+        this.props.location.pathname !== '/diffusing' &&
+        <h1 className="App-title">TICK TOC</h1>
+      }
         <Switch>
-          <Route exact path="/" component={Login} />
+          <Route exact path="/" component={Main} />
           {/* ^^ Will render main-menu or Login, if logged in or not */}
-          <Route exact path="/new-game" component={Login}/> 
+          <Route exact path="/new-game" component={NewGame}/> 
           {/* ^^^ Will render game menu / game options */}
           <Route exact path="/previous-games" component={Login}/> 
           {/* ^^^ Will render previous games */}
-          <Route exact path="/leaderboards" component={Login}/> 
-          {/* ^^^ Will render leaderboards*/}
+          <Route exact path="/leaderboard" component={Login}/> 
+          {/* ^^^ Will render leaderboard*/}
           <Route exact path="/diffusing" component={Bomb} />
 
           <Route exact path="/recap" component={Bomb} />
@@ -37,4 +43,4 @@ class App extends Component {
   Conditional Checking if isLogged or not
 */}
 
-export default App
+export default withRouter(App)
