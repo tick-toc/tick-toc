68b00e43bb48474e617c668fbb762fb4439def64
Evan Alto <evanalto@Evans-MacBook.local>
2019-01-29 16:30:13 -0600

    put targetList on state

--- src/components/Bomb/Bomb.js
+++ src/components/Bomb/Bomb.js
@@ -45,7 +45,8 @@ class Bomb extends Component {
       singleSecond: 0,
       box: {},
       clock: {},
-      module1: {}
+      module1: {},
+      targetList: []
     }
 
 
@@ -63,7 +64,6 @@ class Bomb extends Component {
   componentDidMount() {
 
     var camera, scene, renderer, box, clock, digital, module1, module2;
-    var targetList = [];
     var projector, mouse = { x: 0, y: 0 };
 
     init(this);
@@ -233,7 +233,9 @@ class Bomb extends Component {
           } else {
             wire.userData = { correct: false }
           }
-          targetList.push(wire)
+          THIS.setState(prevState => ({
+            targetList: [ ...prevState.targetList, wire]
+          }))
         })
 
         module1.traverse((o) => {
@@ -362,6 +364,8 @@ class Bomb extends Component {
     }
 
     function onDocumentMouseDown(event, THIS) {
+      const { minute, tenSecond, singleSecond} = THIS.state
+      const times = [minute,tenSecond,singleSecond]
       // the following line would stop any other event handler from firing
       // (such as the mouse's TrackballControls)
       // event.preventDefault();
@@ -377,9 +381,13 @@ class Bomb extends Component {
       projector.unprojectVector(vector, camera);
       var ray = new THREE.Raycaster(camera.position, vector.sub(camera.position).normalize());
       // create an array containing all objects in the scene with which the ray intersects
-      var intersects = ray.intersectObjects(targetList);
+      var intersects = ray.intersectObjects(THIS.state.targetList);
       // if there is one (or more) intersections
+      console.log(intersects,'<<INTERSECTS')
       if (intersects.length > 0) {
+        if (THIS.state.targetList.includes(intersects[0].object)) {
+          console.log("YOOOO")
+        }
         THIS.handleSOW(intersects[0].object.userData)
         module1.remove(intersects[0].object)
       }
@@ -404,6 +412,10 @@ class Bomb extends Component {
   }
 
   componentDidUpdate(prevProps, prevState) {
+
+    // if (this.state.count === 0) {
+    //   // dispatch action to 
+    // }
 
     if (prevState.strikeCount < this.state.strikeCount) {
       if (this.state.strikeCount === this.state.strikesAllowed) console.log('GAME OVER')
